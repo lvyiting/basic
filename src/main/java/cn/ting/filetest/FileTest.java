264056e4f80ea4906d72b244e7810cef73683361
lvyiting <907167912@qq.com>
2025-07-23 17:44:25 +0800

    提交

--- src/main/java/cn/ting/filetest/FileTest.java
+++ src/main/java/cn/ting/filetest/FileTest.java
@@ -1,15 +1,6 @@
 package cn.ting.filetest;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-
-import cn.hutool.core.io.FileUtil;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 
 /**
  * @author : lvyiting
@@ -17,19 +8,19 @@ import org.springframework.core.io.ResourceLoader;
  **/
 public class FileTest {
 	public static void main(String[] args) throws IOException {
-		File temp = new File("D:/temp");
-		if (!temp.exists()){
-			//新建目录
-			temp.mkdir();
-		}
-		File file = new File("D:/temp/apiclient_key.pem");
-		if (!file.exists()){
-			//读取resources下的apiclient_key.pem写入文件中
-			Resource resource = new DefaultResourceLoader().getResource(ResourceLoader.CLASSPATH_URL_PREFIX);
-			InputStream inputStream = resource.getInputStream();
-			FileOutputStream outputStream = new FileOutputStream(file);
-			BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
-//			bufferedOutputStream.write(bufferedOutputStream.);
-		}
+//		File temp = new File("D:/temp");
+//		if (!temp.exists()){
+//			//新建目录
+//			temp.mkdir();
+//		}
+//		File file = new File("D:/temp/apiclient_key.pem");
+//		if (!file.exists()){
+//			//读取resources下的apiclient_key.pem写入文件中
+//			Resource resource = new DefaultResourceLoader().getResource(ResourceLoader.CLASSPATH_URL_PREFIX);
+//			InputStream inputStream = resource.getInputStream();
+//			FileOutputStream outputStream = new FileOutputStream(file);
+//			BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+////			bufferedOutputStream.write(bufferedOutputStream.);
+//		}
 	}
 }
